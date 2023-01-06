5e5b205dc06b8bb7249aa49a00f66ce67e9eaf67
Arturo Serrato <arturo.serrato@yahoo.com>
2023-01-05 22:37:15 -0800

    updated .js codes

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,28 +1,31 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require('./Product.js');
+const Category = require('./Category.js');
+const Tag = require('./Tag.js');
+const ProductTag = require('./ProductTag.js');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
-  onDelete: 'CASCADE',
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
+  onDelete: 'CASCADE',
 });
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongToMany(Tag, {
-  through: ProductTag,
-  foreignKey: 'product_id',
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+  unique: false
+  },
+  as: 'product_tags'
 });
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongToMany(Product, {
+Tag.belongsToMany(Product, {
   through: ProductTag,
   foreignKey: 'tag_id',
 });
